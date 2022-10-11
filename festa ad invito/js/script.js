6ce22f67cc1b4daa1ac8279052c97465e6e9a412
LuongoAndrea <andrea.luongo2002@gmail.com>
2022-10-12 01:25:37 +0200

    festa ad invito

---
+++ festa ad invito/js/script.js
@@ -0,0 +1,23 @@
+const listaInvitati = ["andrea@gmail.com", "francesco@gmail.com", "pippo@gmail.com"];
+
+const element = document.getElementById("btn");
+element.addEventListener('click', function(){
+        let email = document.getElementById("email").value;
+        console.log(email);
+        let invito = false;
+        for (let i = 0; i < listaInvitati.length; i++) {
+            let inviti = listaInvitati[i];
+            console.log("nome lista ciclto " + inviti)
+            if(email == inviti){
+                invito = true;
+            }
+
+            if(invito == true){
+                document.getElementById("messaggio-invito").innerHTML = "ottimo sei invitato";
+            }
+            else(
+                document.getElementById("messaggio-invito").innerHTML = "mi spiace non sei invitato"
+            )
+        }
+    }
+)
